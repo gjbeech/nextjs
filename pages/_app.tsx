842764c15be7a734b9c002a3939918511ef3e737
Graham Beech <Graham.Beech@rcn.org.uk>
2021-12-07 13:45:04 +0000

    updated app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
